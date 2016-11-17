30705c3acf3cc41d39b1d9e5a0b3a837163533a7
Sherah Smith <ssmith@wikimedia.org>
2016-11-16 20:58:09 -0800

    first version. aww.

---
+++ sitewarning.js
@@ -0,0 +1,38 @@
+// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+//   No tabs or host permissions needed!
+//   console.log('Turning ' + tab.url + ' red!');
+//   chrome.tabs.executeScript({
+//     code: 'document.body.style.backgroundColor="red"'
+//   });
+// });
+
+chrome.tabs.onUpdated.addListener(
+function ( tabId, changeInfo, tab )
+{
+  var fakeNewsURLs = getFakeNewsURLs();
+
+  if ( changeInfo.status === "complete" )
+    {
+        //lowercase
+        theURL = tab.url;
+        var scrubbedURL = (theURL).toLowerCase();
+
+        //no http's or www's
+        scrubbedURL = scrubbedURL.replace("https://www.", "");
+        scrubbedURL = scrubbedURL.replace("http://www.", "");
+        scrubbedURL = scrubbedURL.replace("https://", "");
+        scrubbedURL = scrubbedURL.replace("http://", "");
+
+        //no backslashes
+        scrubbedURL = scrubbedURL.split("/")[0];
+
+        if ( fakeNewsURLs.indexOf(scrubbedURL) > -1 )  {
+          warnAboutFakeNews();
+        }
+    }
+}
+);
+
+function warnAboutFakeNews(){
+  alert('Warning! The "news" you\'re about to read is fake.');
+}
